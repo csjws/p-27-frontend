7ef214c54cab2c6bbb0dd3983fbdac643a5ea8ec
JunWoo Seo <csjws@naver.com>
2026-03-17 12:25:49 +0900

    feat: PostDto 구현

--- p-27/frontend/src/app/posts/[id]/page.tsx
+++ p-27/frontend/src/app/posts/[id]/page.tsx
@@ -1,11 +1,11 @@
 "use client";
 import { useParams } from "next/navigation";
 import { useEffect, useState } from "react";
-import { Post } from "../page";
+import { PostDto } from "@/type/post";
 
 export default function Home() {
 
-    const [post, setPost] = useState<Post | null>(null);
+    const [post, setPost] = useState<PostDto | null>(null);
     const { id } = useParams();
 
     useEffect(() => {
